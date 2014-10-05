b612d5a13d9493b2da0871d2f44c95eb9dad485e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-10-05 16:33:11 +0200

    YES astropy init

--- FITS_tools/__init__.py
+++ FITS_tools/__init__.py
@@ -10,7 +10,7 @@ This is an Astropy affiliated package.
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-#from ._astropy_init import *
+from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
