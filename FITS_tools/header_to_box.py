9e317eafc7e67abfb8e62af841c1d44c76b0dbb4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-10-05 10:19:08 +0200

    attempt to build enclosing-header logic

--- FITS_tools/header_to_box.py
+++ FITS_tools/header_to_box.py
@@ -1,10 +1,12 @@
 import numpy as np
+from astropy import wcs
 
 def header_to_box(header):
     """
     Given a flat header (2 dimensions in world coordinates),
     return a box defined by xcenter, ycenter, height, width
     """
+    w = wcs.WCS(header).sub([wcs.WCSSUB_CELESTIAL])
 
     cd1 = header.get('CDELT1') or header.get('CD1_1')
     cd2 = header.get('CDELT2') or header.get('CD2_2')
@@ -12,10 +14,15 @@ def header_to_box(header):
     height = cd2 * header['NAXIS2']
     width = cd1 * header['NAXIS1']
 
-    xcenter = header['CRVAL1'] + ((header['NAXIS1']-1)/2 + header['CRPIX1'])*cd1
-    ycenter = header['CRVAL2'] + ((header['NAXIS2']-1)/2 + header['CRPIX2'])*cd2
+    #xcenter = header['CRVAL1'] + ((header['NAXIS1']-1)/2 + header['CRPIX1'])*cd1
+    #ycenter = header['CRVAL2'] + ((header['NAXIS2']-1)/2 + header['CRPIX2'])*cd2
+    (xcenter,ycenter), = w.wcs_pix2world([[(header['NAXIS1']-1)/2.,
+                                           (header['NAXIS2']-1)/2.]], 0)
 
     return xcenter,ycenter,height,width
 
 def box_to_header(xcenter,ycenter,height,width,cd1,cd2):
     raise NotImplementedError
+
+def header_to_ds9reg(header):
+    return "box({0}, {1}, {3}, {2}, 0)".format(*header_to_box(header))
--- FITS_tools/header_tools.py
+++ FITS_tools/header_tools.py
@@ -1,9 +1,12 @@
 from astropy import wcs
 import numpy as np
 from astropy import units as u
+from astropy import coordinates
 from astropy.wcs import WCSSUB_CELESTIAL
+from astropy import log
 
 from .load_header import get_cd
+from .hcongrid import _ctype_to_csys
 
 def header_to_platescale(header, **kwargs):
     """
@@ -89,3 +92,70 @@ def smoothing_kernel_size(hdr_from, hdr_to):
             widths[ii] = np.abs(cd_to/cd_from) / np.sqrt(8*np.log(2))
 
     return widths
+
+def enclosing_header(header1, header2, wrapangle=180*u.deg):
+    """
+    Find the smallest header that encloses both header1 and header2 in the
+    frame of header2
+    """
+    log.warning("Does not work exactly - there is at least a few-pixel offset"
+                " in the result.  Need a good test suite.")
+    w1 = wcs.WCS(header1).sub([WCSSUB_CELESTIAL])
+    w2 = wcs.WCS(header2).sub([WCSSUB_CELESTIAL])
+
+    pedges1 = np.array([(1, header1['NAXIS2']),
+                        (header1['NAXIS1'], header1['NAXIS2']),
+                        (header1['NAXIS1'], 1),
+                        (1, 1),])
+    pedges2 = np.array([(1,header2['NAXIS2']),
+                        (header2['NAXIS1'], header2['NAXIS2']),
+                        (header2['NAXIS1'], 1),
+                        (1, 1),])
+    edges1 = w1.wcs_pix2world(pedges1, 1)
+    edges2 = w2.wcs_pix2world(pedges2, 1)
+
+    cedges2 = coordinates.SkyCoord(edges2*u.deg,
+                                   frame=_ctype_to_csys(w1.wcs))
+
+    cedges1 = coordinates.SkyCoord(edges1*u.deg,
+                                   frame=_ctype_to_csys(w2.wcs)).transform_to(cedges2.frame)
+
+    #(a,b), = w2.wcs_pix2world([[1,1]],1)
+    #lbref = coordinates.SkyCoord(a*u.deg, b*u.deg,
+    #                             frame=_ctype_to_csys(w2.wcs))
+
+    wa = wrapangle
+    if hasattr(cedges2, 'l'):
+        llow = min(cedges1.l.wrap_at(wa).min(), cedges2.l.wrap_at(wa).min())
+        lhi  = max(cedges1.l.wrap_at(wa).max(), cedges2.l.wrap_at(wa).max())
+        blow = min(cedges1.b.wrap_at(wa).min(), cedges2.b.wrap_at(wa).min())
+        bhi  = max(cedges1.b.wrap_at(wa).max(), cedges2.b.wrap_at(wa).max())
+        #lref, bref = lbref.l, lbref.b
+    elif hasattr(cedges2, 'ra'):
+        llow = min(cedges1.ra.min(),  cedges2.ra.min())
+        lhi  = max(cedges1.ra.max(),  cedges2.ra.max())
+        blow = min(cedges1.dec.min(), cedges2.dec.min())
+        bhi  = max(cedges1.dec.max(), cedges2.dec.max())
+        #lref, bref = lbref.ra, lbref.dec
+    else:
+        raise ValueError("Invalid coordinates.")
+
+
+    (xlow,ylow), = w2.wcs_world2pix([[llow.deg,blow.deg]], 1)
+    (xhi ,yhi ), = w2.wcs_world2pix([[lhi.deg ,bhi.deg ]], 1)
+
+    (xref, yref), = w2.wcs_world2pix([[(max(llow,lhi)).deg,
+                                       (min(blow,bhi)).deg]], 0)
+    print cedges1, cedges2
+    print llow.deg, lhi.deg
+    print blow.deg, bhi.deg
+    print xref, yref
+    print int(np.ceil(np.abs(xhi-xlow))),int(np.ceil(np.abs(yhi-ylow)))
+
+    header = header2.copy()
+    header['NAXIS1'] = int(np.ceil(np.abs(xhi-xlow)))
+    header['NAXIS2'] = int(np.ceil(np.abs(yhi-ylow)))
+    header['CRPIX1'] = header2['CRPIX1'] - xref #min(xlow, xhi)
+    header['CRPIX2'] = header2['CRPIX2'] - yref #min(ylow, yhi)
+
+    return header
