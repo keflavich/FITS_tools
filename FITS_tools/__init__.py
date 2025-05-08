08532fa3cb267131a0b8e91db010893858b63abf
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2025-05-08 06:43:35 -0400

    factor out more deprecated stuff

--- FITS_tools/__init__.py
+++ FITS_tools/__init__.py
@@ -11,34 +11,9 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-
-    import os
-    from warnings import warn
-    from astropy import config
-
-    # add these here so we only need to cleanup the namespace at the end
-    config_dir = None
-
-    if not os.environ.get('ASTROPY_SKIP_CONFIG_UPDATE', False):
-        config_dir = os.path.dirname(__file__)
-        try:
-            config.configuration.update_default_config(__package__, config_dir)
-        except config.configuration.ConfigurationDefaultMissingError as e:
-            wmsg = (e.args[0] + " Cannot install default profile. If you are "
-                    "importing from source, this is expected.")
-            warn(config.configuration.ConfigurationDefaultMissingWarning(wmsg))
-            del e
-        except AttributeError as e:
-            wmsg = (e.args[0] + ", which has been deprecated in astropy 6.1.1.")
-            warn(wmsg)
-            del e
-
-    del os, warn, config_dir  # clean up namespace
-
     from .match_images import project_to_header,match_fits
     from .fits_overlap import fits_overlap,header_overlap
     from . import hcongrid,spectral_regrid
     from .cube_regrid import regrid_fits_cube,regrid_cube_hdu,regrid_cube
     from . import header_tools
     from . import spectral_regrid
-
